8f04b43e720319daf8538140ebbe75910831f5ef
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2026-01-24 19:24:26 +0000

    Set catppuccin as default theme and update plugins
    Configured LazyVim to use the 'catppuccin-mocha' theme by default and removed the custom catppuccin plugin config. Renamed 'copilot.lua' to 'ai.lua' and added a new dashboard plugin configuration using 'folke/snacks.nvim'.

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -14,23 +14,24 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local theme = "catppuccin-mocha"
+
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = theme
+      },
+      import = "lazyvim.plugins"
+    },
     { import = "plugins" },
   },
+  install = { colorscheme = { theme } },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    version = false,
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
