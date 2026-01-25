cf8bd934830c1f6695dc29bf2fc215c6be529e2e
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2026-01-25 22:09:38 +0000

    Update terminal config and disable arrow keys in Neovim
    Reduced cell height in Ghostty config for improved appearance. Disabled arrow keys in Neovim to encourage hjkl navigation practice.

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Disable arrow keys to practice hjkl navigation
+vim.keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>", { desc = "Disabled" })
+vim.keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>", { desc = "Disabled" })
+vim.keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>", { desc = "Disabled" })
+vim.keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>", { desc = "Disabled" })
